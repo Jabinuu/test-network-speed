17b83832894236f7c77e0f384918c78545bac45b
jiabin <1123565780@qq.com>
2023-06-03 19:44:44 +0800

    测试函数封装

--- web/network-speed-test/src/App.vue
+++ web/network-speed-test/src/App.vue
@@ -17,20 +17,27 @@ import { ref, resolveDirective, watch } from 'vue'
 import nst from '../../../network-speed-test/dist/index'
 import 'ant-design-vue/lib/button/style'
 import AButton from 'ant-design-vue/lib/button'
-const isLoading = ref(false)
-const isError = ref(false)
-const isStart = ref(false)
-const disabled = ref(false)
-const speed = ref(0)
+const isLoading = ref(false) //正在测速的状态变量
+const isError = ref(false) //是否捕获到错误
+const isStart = ref(false) //第一次测速
+const disabled = ref(false) //正在测速时，禁用测速按钮
+const speed = ref(0) //测得的网速值
 
-watch(isLoading, async () => {
+watch(isLoading, getNetworkSpeed)
+
+async function getNetworkSpeed() {
   if (isLoading.value) {
     try {
+      // 图片探测方式测速
       // speed.value = await nst.getSpeedByImg(
       //   'https://pic4.zhimg.com/v2-392227d18fe64dd8950e2b18eb279ecf_r.jpg',
       //   734208
       // )
+
+      // Ajax方式测速
       speed.value = await nst.getSpeedByAjax('/api/v2-479db8ce71c66355d06f94b6ee9fee06_r.jpg')
+
+      // 开发环境下，启用DevTools => 网络 => 勾选停用缓存，可以不经代理进行测试
       // speed.value = await nst.getSpeedByAjax(
       //   'https://pic3.zhimg.com/v2-479db8ce71c66355d06f94b6ee9fee06_r.jpg'
       // )
@@ -41,7 +48,8 @@ watch(isLoading, async () => {
     isLoading.value = false
     disabled.value = false
   }
-})
+}
+
 function onClickStart() {
   isError.value = false
   disabled.value = true
